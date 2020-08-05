bf7988bb43eb144949092dd1cbb5338dec32d627
Ivan Dubrovin <62808937+velom-git@users.noreply.github.com>
2020-08-05 17:04:41 +0300

    Update CRUD.java

--- src/java/CRUD.java
+++ src/java/CRUD.java
@@ -7,16 +7,16 @@ public class CRUD {
 
     public static void create(String name, int age) throws SQLException {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/crud?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
-                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456"); Statement statement = conn.createStatement()) {
+                "jdbc:mysql://localhost:3306/bd?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
+                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "login", "password"); Statement statement = conn.createStatement()) {
             statement.executeUpdate("insert person (name,age) values ('" + name + "'," + age + ")");
         }
     }
 
     public static void read(int id) throws SQLException {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/crud?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
-                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456"); Statement statement = conn.createStatement()) {
+                "jdbc:mysql://localhost:3306/bd?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
+                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "login", "password"); Statement statement = conn.createStatement()) {
             ResultSet resultSet = statement.executeQuery("select * from person where id = " + id);
             while (resultSet.next()) {
                 System.out.print(resultSet.getString(2) + " ");
@@ -27,24 +27,24 @@ public class CRUD {
 
     public static void update(int id, String newName, int newAge) throws SQLException {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/crud?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
-                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456"); Statement statement = conn.createStatement()) {
+                "jdbc:mysql://localhost:3306/bd?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
+                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "login", "password"); Statement statement = conn.createStatement()) {
             statement.executeUpdate("replace person values (" + id + ", '" + newName + "', " + newAge + ")");
         }
     }
 
     public static void delete(String name) throws SQLException {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/crud?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
-                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456"); Statement statement = conn.createStatement()) {
+                "jdbc:mysql://localhost:3306/bd?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
+                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "login", "password"); Statement statement = conn.createStatement()) {
             statement.executeUpdate("delete from person where name = '" + name + "'");
         }
     }
 
     public static void delete(int id) throws SQLException {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/crud?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
-                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "123456"); Statement statement = conn.createStatement()) {
+                "jdbc:mysql://localhost:3306/bd?verifyServerCertificate=false&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShi" +
+                        "ft=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "login", "password"); Statement statement = conn.createStatement()) {
             statement.executeUpdate("delete from person where id = '" + id + "'");
         }
     }
